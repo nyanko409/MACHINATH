e9e5a2e27cc481a928aebf7008b72a76dd9ca69e
betaleon <leonbeta0527@gmail.com>
2019-12-17 18:21:42 +0900

    title fuck

--- MACHINATH/MACHINATH/Title.cpp
+++ MACHINATH/MACHINATH/Title.cpp
@@ -15,9 +15,9 @@ static Sprite titleSprite;
 void InitTitle()
 {
 	// init title sprite
-	titleSprite = Sprite(Texture_GetTexture(TEXTURE_INDEX_TITLE), D3DXVECTOR3(0, 0, 0),
+	titleSprite = Sprite(Texture_GetTexture(TEXTURE_INDEX_TITLE), D3DXVECTOR3(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 0),
 		D3DXVECTOR3(Texture_GetWidth(TEXTURE_INDEX_TITLE) / 2, Texture_GetHeight(TEXTURE_INDEX_TITLE) / 2, 0),
-		0, D3DXVECTOR2(1, 1), D3DCOLOR_RGBA(255, 255, 255, 255));
+		0, D3DXVECTOR2(1.92f/2.0f, 1.08f/2.0f), D3DCOLOR_RGBA(255, 255, 255, 255));
 }
 
 void UpdateTitle()
